196deddaed950cb554fde16538ec6b755bf27cfc
Sophie von Fromm <sfromm@dartmouth.edu>
2024-08-16 08:04:00 -0400

    updated metal data

--- Code/SelectiveMetals.R
+++ Code/SelectiveMetals.R
@@ -6,7 +6,7 @@
 library(tidyverse)
 
 # Load HBEF data
-HBEF_data <- read_csv("./Data/HBEF_data_all_2024-08-06.csv") %>% 
+HBEF_data <- read_csv("./Data/HBEF_data_all_2024-08-15.csv") %>% 
   # not needed once field notes are entered
   dplyr::select(-c(Plot_1_Horizons:Notes))
 
@@ -16,47 +16,87 @@ HBEF_data$Horizon <- factor(HBEF_data$Horizon,
                             levels = c("Oi/Oe", "Oa/A", "Mineral_0_10"),
                             ordered = TRUE)
 
+#Function to plot metal data over time by horizon
+fun_metal_time <- function(metal){
+  HBEF_data %>% 
+    drop_na(.data[[metal]]) %>% 
+    ggplot(aes(x = Year, y = .data[[metal]])) +
+    geom_point(aes(color = Plot), size = 2) +
+    theme_classic(base_size = 16) +
+    theme(axis.text = element_text(color = "black")) +
+    facet_wrap(~Horizon) +
+    geom_smooth(method = "lm") +
+    scale_x_continuous("", limits = c(1998,2023))
+}
 
-HBEF_data %>% 
-  drop_na(al_pyr_mg_g) %>% 
-  ggplot(aes(x = Year, y = al_pyr_mg_g)) +
-  geom_point(aes(color = Plot), size = 2) +
-  theme_classic(base_size = 16) +
-  theme(axis.text = element_text(color = "black")) +
-  facet_wrap(~Horizon) +
-  geom_smooth(method = "lm") +
-  scale_x_continuous("", limits = c(1998,2023)) +
-  scale_y_continuous("Pyrophosphate extracted Al [mg/g]")
+#pyro Al
+fun_metal_time("al_pyr_mg_g") +
+  scale_y_continuous("Pyrophosphate extracted Al [mg/g]", limits = c(0,18),
+                     expand = c(0,0))
 
-HBEF_data %>% 
-  drop_na(al_pyr_mg_g, `Measured_%_C`) %>% 
-  ggplot(aes(y = `Measured_%_C`*10, x = al_pyr_mg_g)) +
-  geom_point(aes(color = Plot), size = 2) +
-  theme_classic(base_size = 16) +
-  theme(axis.text = element_text(color = "black")) +
-  facet_wrap(~Horizon) +
-  geom_smooth(method = "lm") +
-  scale_y_continuous("Total C [mg/g]") +
-  scale_x_continuous("Pyrophosphate extracted Al [mg/g]")
+#pyro Fe
+fun_metal_time("fe_pyr_mg_g") +
+  scale_y_continuous("Pyrophosphate extracted Fe [mg/g]", limits = c(0,16),
+                     expand = c(0,0))
+
+#oxal Al
+fun_metal_time("al_ox_mg_g") +
+  scale_y_continuous("Oxalate extracted Al [mg/g]", limits = c(0,18),
+                     expand = c(0,0))
+
+#oxal Fe
+fun_metal_time("fe_ox_mg_g") +
+  scale_y_continuous("Oxalate extracted Fe [mg/g]", limits = c(0,16),
+                     expand = c(0,0))
+
+#Function to plot metal data against by horizon
+fun_metal_oc <- function(metal){
+  HBEF_data %>% 
+    drop_na(.data[[metal]], `Measured_%_C`) %>% 
+    ggplot(aes(y = `Measured_%_C`*10, x = .data[[metal]])) +
+    geom_point(aes(color = Plot), size = 2) +
+    theme_classic(base_size = 16) +
+    theme(axis.text = element_text(color = "black")) +
+    facet_wrap(~Horizon) +
+    geom_smooth(method = "lm") +
+    scale_y_continuous("Total C [mg/g]")
+}
+
+#pyro Al
+fun_metal_oc("al_pyr_mg_g") +
+  scale_x_continuous("Pyrophosphate extracted Al [mg/g]", limits = c(0,18),
+                     expand = c(0,0))
+
+#pyro Fe
+fun_metal_oc("fe_pyr_mg_g") +
+  scale_x_continuous("Pyrophosphate extracted Fe [mg/g]", limits = c(0,16),
+                     expand = c(0,0))
+
+#oxal Al
+fun_metal_oc("al_ox_mg_g") +
+  scale_x_continuous("Oxalate extracted Al [mg/g]", limits = c(0,18),
+                     expand = c(0,0))
+
+#oxal Fe
+fun_metal_oc("fe_ox_mg_g") +
+  scale_x_continuous("Oxalate extracted Fe [mg/g]", limits = c(0,16),
+                     expand = c(0,0))
+
+
+
 
 HBEF_data %>% 
   drop_na(fe_pyr_mg_g) %>% 
-  ggplot(aes(x = Year, y = fe_pyr_mg_g)) +
-  geom_point(aes(color = Plot), size = 2) +
+  group_by(Horizon, Year) %>% 
+  summarise(mean = mean(fe_pyr_mg_g),
+            sd = sd(fe_pyr_mg_g)) %>% 
+  ggplot(aes(x = Year)) +
+  geom_point(aes(y = mean), size = 2) +
+  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.5) +
   theme_classic(base_size = 16) +
   theme(axis.text = element_text(color = "black")) +
   facet_wrap(~Horizon) +
-  geom_smooth(method = "lm") +
   scale_x_continuous("", limits = c(1998,2023)) +
   scale_y_continuous("Pyrophosphate extracted Fe [mg/g]")
 
-HBEF_data %>% 
-  drop_na(fe_pyr_mg_g, `Measured_%_C`) %>% 
-  ggplot(aes(y = `Measured_%_C`*10, x = fe_pyr_mg_g)) +
-  geom_point(aes(color = Plot), size = 2) +
-  theme_classic(base_size = 16) +
-  theme(axis.text = element_text(color = "black")) +
-  facet_wrap(~Horizon) +
-  geom_smooth(method = "lm") +
-  scale_y_continuous("Total C [mg/g]") +
-  scale_x_continuous("Pyrophosphate extracted Fe [mg/g]")
+
